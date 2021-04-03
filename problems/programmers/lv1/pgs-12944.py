e8576860e70c7b5488325f8dfb1978efa15df92f
CodyBuilder-devr <imspecial1@u.sogang.ac.kr>
2021-04-04 00:53:43 +0900

    프로그래머스 레벨1 총정리

---
+++ problems/programmers/lv1/pgs-12944.py
@@ -0,0 +1,7 @@
+"""
+제목 : 평균 구하기
+
+아이디어 : 그냥 계산하면 된다
+"""
+def solution(arr):
+    return sum(arr) / len(arr)
--- problems/programmers/lv1/pgs-12947.py
+++ problems/programmers/lv1/pgs-12947.py
@@ -1,6 +1,16 @@
 """
 제목 : 하샤드 수
+
+아이디어 : 그냥 구현하면 됨
+
+구현 :
+- 최대한 짧게 구현해보자
 """
 
 def solution(x) :
-    
+    return True if x%sum([int(char) for char in str(x)])==0 else False
+
+print(solution(10))
+print(solution(12))
+print(solution(11))
+print(solution(13))
