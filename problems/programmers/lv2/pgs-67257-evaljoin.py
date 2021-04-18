bf063ca87b2c33d36e3176b5c80cabe7b66e4c43
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-04-19 00:29:02 +0900

    수식의 join을 통한 괄호 추가

---
+++ problems/programmers/lv2/pgs-67257-evaljoin.py
@@ -0,0 +1,18 @@
+def solution(expression):
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
+
+# 테스트 케이스
+print(solution("100-200*300-500+20"),60420)
+print(solution("177-661*999*99-133+221+334+555-166-144-551-166*166-166*166-133*88*55-11*4+55*888*454*12+11-66+444*99"),6083974714)
+print(solution("2-990-5+2"),995)
+print(solution("50*6-3*2"),300)
