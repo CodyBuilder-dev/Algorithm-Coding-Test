acf44d327b70babf6fceef54e832715aa8b6445d
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-04-24 10:02:44 +0900

    조이스틱

--- problems/programmers/lv2/pgs-42860-wrong.py
+++ problems/programmers/lv2/pgs-42860-wrong.py
@@ -1,35 +1,72 @@
 """
-접근법 : 
-알파벳의 왼쪽에서 오른쪽으로 진행하며, 그때그때 최선의 선택을 한다
-1. 포인터를 옮긴다. 
-2. 알파벳이 같으면 pass
-2. 알파벳이 다르면 포인터 선택
-3. 현재 포인터에서 다음 포인터까지 최단거리 선택
-3. 도착하면 알파벳 최단거리 선택
+제목 : 조이스틱
 
--> 문제점 : 왼->오가 최고 빠르다는 보장이 없음
+아이디어 : 조이스틱이 좌우로 자유롭게 이동가능하다고 가정
+(1) 수평으로 가장 가까운 NOT A 버튼으로 이동
+- 왼쪽/오른쪽으로 보내본다
+    - 왼쪽으로 가장 최초로 NOT A가 나올떄까지의 거리를 찾는다
+    - 오른쪽도 마찬가지
+- 계속 가서 한바퀴(len)이상 찾아도 NOT A 안나오면 문자열이 모두 A인 경우
+    - 0 반환 후 종료
+- 둘중 더 가까운 칸으로 이동한다
+- 어느 칸으로 갈지 결정되면, 이동거리를 계산한다
+    - 거리가 len//2 초과이면 반대로 가는게 빠르다
+    - 거리가 len//2 이면 동일하다
+    - 거리가 len//2 미만이면 원래대로 가는게 맞다
 
+(2) 수직 이동거리 계산
+- 최소거리 조건
+    - 거리가 27//2 = 13 초과이면 반대로 가는게 빠르다
+    - 거리가 13이면 위아래 동일하다
+    - 거리가 13미만이면 제대로 가는게 빠르다
+- 구현
+    - min(x,26-x)으로 처리
 """
+from math import inf
 
-alphaStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def choice_lr(name,index) :
+    # right
+    right_len = 0
+    i,j = index,index
+    while name[i] == 'A':
+        if (right_len) > len(name):
+            return -inf
+        i = (i+1) % len(name)
+        right_len += 1
+
+    # left
+    left_len = 0
+    while name[j] == 'A':
+        if (left_len) > len(name):
+            return -inf
+        j = (j - 1) % len(name)
+        left_len += 1
+    return -1*left_len if left_len < right_len else right_len
 
 def solution(name):
-    aName = "A"*len(name)
+    name = [x for x in name]
 
-    cnt = 0
-    pt = 0
-    for i,v in enumerate(name) :
-        if name[i] == aName[i] :
-            continue
-        else :
-            cnt += min(i-pt,len(name)-i+pt)
-            charDiff = max(ord(name[i])-ord(aName[i]), ord(aName[i])-ord(name[i]))
-            cnt += min(26-charDiff,charDiff)
-            pt = i
-    return cnt
+    point = 0
+    total_move = 0
+    while True :
+        # 좌우 이동
+        direction =choice_lr(name,point)
+        if direction == -inf: break
+        point = (point + direction) % len(name)
+        total_move += abs(direction)
+
+        #상하 이동
+        dist_ud = abs(ord(name[point])-ord("A"))
+        total_move += min(dist_ud,26-dist_ud)
+        name[point] = "A"
 
 
-name = "JEROEN"
-name = "JAN"
-name = "JAZ"
-print(solution(name))
+    answer = total_move
+    return answer
+
+#테스트 케이스, 설명대로의 정답, 실제 테스트케이스의 정답
+print(solution("DAZ"),5,5)
+print(solution("ABAAAAAAAAABB"),7,7)
+print(solution("JAJAAAJ"),32, 31)
+
+
--- problems/programmers/lv2/pgs-42860.py
+++ problems/programmers/lv2/pgs-42860.py
@@ -1,75 +1,73 @@
 """
 제목 : 조이스틱
 
-아이디어 :
-- A인 거는 가만히 놔둔다
-- A가 아닌 다음 놈의 거리를 구한다
+아이디어 : 조이스틱이 우측에서는 이동할 수 없다고 가정
+(1) 수평으로 가장 가까운 NOT A 버튼으로 이동
+- 왼쪽/오른쪽으로 보내본다
+    - 왼쪽으로 가장 최초로 NOT A가 나올떄까지의 거리를 찾는다
+    - 오른쪽도 마찬가지
+- 계속 가서 한바퀴(len)이상 찾아도 NOT A 안나오면 문자열이 모두 A인 경우
+    - 0 반환 후 종료
+- 둘중 더 가까운 칸으로 이동한다
+- 어느 칸으로 갈지 결정되면, 이동거리를 계산한다
     - 거리가 len//2 초과이면 반대로 가는게 빠르다
     - 거리가 len//2 이면 동일하다
     - 거리가 len//2 미만이면 원래대로 가는게 맞다
 
-- 위아래 거리를 구한다
+(2) 수직 이동거리 계산
+- 최소거리 조건
     - 거리가 27//2 = 13 초과이면 반대로 가는게 빠르다
-        - AB
     - 거리가 13이면 위아래 동일하다
     - 거리가 13미만이면 제대로 가는게 빠르다
-
-구현 :
-세로거리 구하기 함수
-- A와 B 크기 비교
-    - B가 크면 B-A반환
-    - A가 크면 B+27-A반환
-
-가로축 제일 가까운 NOT A버튼으로 이동
-- 왼쪽 오른족에 똑같은 문자열 하나씩 붙인다다- 왼쪽으로 보내본다
-    - A아닌거 나올떄까지의 거리를 찾는다
-- 오른쪽으로 보내본다
-    - 마찬가지
-- 계속 가서 한바퀴(len)이상 가도 A아닌게 안나오면 없는거임
-    - 반환 0
+- 구현
+    - min(x,26-x)으로 처리
 """
+from math import inf
+
 def choice_lr(name,index) :
-    # right
-    right_len = 0
+    right_len,left_len = 0,0
     i,j = index,index
-    while name[i] == 'A' :
-        if (right_len) > len(name) :
-            return -987654321
+
+    # right
+    while name[i] == 'A':
+        if (right_len) > len(name):
+            return -inf
+        if i == len(name):
+            right_len = inf
+            break
         i = (i+1) % len(name)
-        right_len +=1
+        right_len += 1
 
     # left
-    left_len = 0
     while name[j] == 'A':
         if (left_len) > len(name):
-            return -987654321
+            return -inf
         j = (j - 1) % len(name)
         left_len += 1
     return -1*left_len if left_len < right_len else right_len
 
 def solution(name):
     name = [x for x in name]
-    visited = [0]*len(name)
 
     point = 0
     total_move = 0
     while True :
         # 좌우 이동
         direction =choice_lr(name,point)
-        if direction == -987654321 : break
+        if direction == -inf: break
         point = (point + direction) % len(name)
         total_move += abs(direction)
 
         #상하 이동
         dist_ud = abs(ord(name[point])-ord("A"))
         total_move += min(dist_ud,26-dist_ud)
-        name[point] ="A"
+        name[point] = "A"
 
 
     answer = total_move
     return answer
 
-#테스트 케이스, 설명대로의 정답, 등록 해답의 정답
+#테스트 케이스, 설명대로의 정답, 실제 테스트케이스의 정답
 print(solution("DAZ"),5,5)
 print(solution("ABAAAAAAAAABB"),7,7)
 print(solution("JAJAAAJ"),32, 31)
