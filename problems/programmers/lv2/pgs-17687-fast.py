befdbb464cbe78b5486806c77c8f66c663bddd10
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-06-27 00:03:19 +0900

    진법변환 함수 구현

--- problems/programmers/lv2/pgs-17687-fast.py
+++ problems/programmers/lv2/pgs-17687-fast.py
@@ -24,7 +24,7 @@ def solution(n, t, m, p):
         answer += dec2n(i,n)
         i += 1
 
-    return ''.join(answer[p-1::m])
+    return ''.join(answer[p-1:m*t:m])
 
 print(solution(2,4,2,1))
 print(solution(16,16,2,1))
