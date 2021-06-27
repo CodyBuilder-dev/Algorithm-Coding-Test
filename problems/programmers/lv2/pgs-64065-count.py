693b586102f317a32d2f31fa3f561f5445bde308
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-06-27 11:12:36 +0900

    정규표현식을 통한 튜플 생성

--- problems/programmers/lv2/pgs-64065-count.py
+++ problems/programmers/lv2/pgs-64065-count.py
@@ -5,10 +5,7 @@
 (1) 일단 문자열을 파싱한다
 - 맨 끝의 {}를 뗀다
 
-(2) 정렬한다
-- 길이순으로 오름차순 정렬한다
-- value를 별도의 order_dict에 넣는다
-- order_dict순서로 정렬한다
+(2) 출현빈도를 센다
 """
 import re
 
@@ -17,14 +14,14 @@ def solution(s):
     s = re.findall('\d+[,\d]*',s)
     s = sorted(s,key=lambda x:len(x))
 
+
     for c in s:
         c = c.split(',')
-        if len(c) == 1: hash[c[0]] = 1
-        else:
-            for x in c:
-                if hash.get(x): continue
-                else: hash[x] = list(hash.values())[-1]+1
-    return list(map(int,hash.keys()))
+        for x in c:
+            if not hash.get(x): hash[x] =0
+            hash[x] += 1
+
+    return list(map(int,sorted(hash, key = lambda x:-hash[x])))
 
 print(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}"))
 print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
