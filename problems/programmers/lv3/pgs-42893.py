a1a646336cb8753facd65c5e5a2769a15b9d39bc
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-07-03 21:35:01 +0900

    정규식을 이용한 HTML파싱

--- problems/programmers/lv3/pgs-42893.py
+++ problems/programmers/lv3/pgs-42893.py
@@ -14,32 +14,79 @@
 """
 import re
 
-class Webpage:
-    def __init__(self,html):
-        self.html = html
-        self.basic_point = len(re.findall(f"\\b{word}\\b",self.html,re.I))
-        self.outward_link = []
-        self.inward_link = []
+def find_url(page):
+    # r = re.search(r"<meta[\w\b\s=:/.-]*>", page)
+    r = re.search(r"<meta[\w\s/.=':-]*https[\w\s/.=':-]*>", page)
+    url = re.search(r"'https.*'",r.group())
 
-    def add_outward_link(self,outward_link):
-        self.outward_link.append(outward_link)
+    return url.group()[1:-1]
 
-    def add_inward_link(self,inward_link):
-        self.outward_link.append(inward_link)
+def find_word(word, page):
+    page = re.sub(r"[^a-zA-Z]","  ",page)
+    matches = re.findall(f"\s{word}\s",page)
+    return len(matches)
 
-    def get_basic_point(self,word):
-        return self.basic_point
 
-    def get_link_point(self):
-        pass
+def find_outbound(word, page):
+    result = []
+    matches = re.findall(r"<a[\w\s/.=':-]*https[\w\s/.=':-]*>", page)
+    for match in matches:
+        url = re.search(r"'https.*'",match)
+        result.append(url.group()[1:-1])
 
+    return result
 
 def solution(word, pages):
-    page_classes = []
-    for page in pages :
-        page_classes.append(Webpage(page))
+    word = word.lower()
+    page_infos = dict()
 
-    return page_classes
+    for i,page in enumerate(pages):
+        # text 전처리
+        page = page.replace('\n', ' ').replace('\"', "'").lower()
 
-# print(re.findall("\\bblind\\b","blind bilnd blinde Blind",re.I))
+        # 페이지 url 파싱
+        url = find_url(page)
+        if url not in page_infos:
+            page_infos[url] = {"index": i, "base_point": 0, "inbound": [], "outbound": 0}
+
+    for i,page in enumerate(pages):
+        # text 전처리
+        page = page.replace('\n',' ').replace('\"',"'").lower()
+        # print(page)
+
+        # check base point
+        page_infos[url]["base_point"] = find_word(word,page)
+
+        # check outbound link
+        outbound_links = find_outbound(word, page)
+        # print("외부로 나가는 링크는",outbound_links)
+        page_infos[url]["outbound"] = len(outbound_links)
+
+        for link in outbound_links:
+            if link in page_infos:
+                page_infos[link]["inbound"].append(url)
+
+    # return page_infos
+    # 점수 계산
+    max_idx = -987654321
+    max_point = -987654321
+
+    for k in page_infos:
+        link_point = 0
+        base_point = page_infos[k]["base_point"]
+        for inbound in page_infos[k]["inbound"]:
+            link_point += page_infos[inbound]["base_point"]/page_infos[inbound]["outbound"]
+        final_point = base_point + link_point
+
+        if final_point > max_point:
+            max_idx = page_infos[k]["index"]
+            max_point = final_point
+
+    return max_idx
+
+
 print(solution("blind",["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blind\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]))
+print(solution("Muzi",["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://careers.kakao.com/interview/list\"/>\n</head>  \n<body>\n<a href=\"https://programmers.co.kr/learn/courses/4673\"></a>#!MuziMuzi!)jayg07con&&\n\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://www.kakaocorp.com\"/>\n</head>  \n<body>\ncon%\tmuzI92apeach&2<a href=\"https://hashcode.co.kr/tos\"></a>\n\n\t^\n</body>\n</html>"]))
+print(solution("blind",["<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://a.com\"/>\n</head>  \n<body>\nBlind Lorem Blind ipsum dolor Blind test sit amet, consectetur adipiscing elit. \n<a href=\"https://b.com\"> Link to b </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://b.com\"/>\n</head>  \n<body>\nSuspendisse potenti. Vivamus venenatis tellus non turpis bibendum, \n<a href=\"https://a.com\"> Link to a </a>\nblind sed congue urna varius. Suspendisse feugiat nisl ligula, quis malesuada felis hendrerit ut.\n<a href=\"https://c.com\"> Link to c </a>\n</body>\n</html>", "<html lang=\"ko\" xml:lang=\"ko\" xmlns=\"http://www.w3.org/1999/xhtml\">\n<head>\n  <meta charset=\"utf-8\">\n  <meta property=\"og:url\" content=\"https://c.com\"/>\n</head>  \n<body>\nUt condimentum urna at felis sodales rutrum. Sed dapibus cursus diam, non interdum nulla tempor nec. Phasellus rutrum enim at orci consectetu blinde\n<a href=\"https://a.com\"> Link to a </a>\n</body>\n</html>"]))
+
+
