4f3770aa973286891a4cd7de49d43c2e86635d3a
Codybuilder-dev <imspecial1@u.sogang.ac.kr>
2021-07-15 09:07:25 +0900

    문자열을 DP로 해결하는 문제

--- problems/programmers/lv4/pgs-12983-slow.py
+++ problems/programmers/lv4/pgs-12983-slow.py
@@ -10,8 +10,6 @@
             , (2자리 단어가 존재시) dp[n-2]+1
             , (3자리 단어가 존재시) dp[n-3]+1
             , ....
-최적화 :
-    - strs의 모든원소의 길이는 1이상 5이하이므로, in을 사용하면 5번만 훑어볼 수 있다
 """
 from math import inf
 
@@ -22,9 +20,9 @@ def solution(strs, t):
     dp[0] = 0
 
     for i,c in enumerate(t, start=1):
-        for j in range(1,6):
-            if t[i - j:i] in strs:
-                dp[i] = dp[i-j] + 1 if dp[i-j] + 1 < dp[i] else dp[i]
+        for word in strs:
+            if word.endswith(c) and t[i - len(word):i] == word:
+                dp[i] = dp[i-len(word)] + 1 if dp[i-len(word)] + 1 < dp[i] else dp[i]
 
 
     return dp[len(t)] if dp[len(t)] != inf else -1
